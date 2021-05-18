ab7a7b7d80b94317e5cf8731ad7cbdf5bb00db9f
Noah Hoffman <noah.hoffman@gmail.com>
2021-05-17 21:56:01 -0700

    add option to limit character set of test codes

--- barcoder/commands/get_codes.py
+++ barcoder/commands/get_codes.py
@@ -6,7 +6,7 @@ import sys
 import argparse
 import csv
 
-from barcoder.utils import generate_codes
+from barcoder.utils import generate_codes, get_code
 
 log = logging.getLogger(__name__)
 
@@ -15,13 +15,18 @@ def build_parser(parser):
     parser.add_argument('-n', '--number', type=int, default=1)
     parser.add_argument('-l', '--length', type=int, default=16)
     parser.add_argument('-s', '--stop-if-seen', action='store_true', default=False)
+    parser.add_argument('--alpha-chars', help='limit to these characters')
     parser.add_argument('-o', '--outfile', help="Output file",
                         default=sys.stdout, type=argparse.FileType('w'))
 
 
 def action(args):
 
-    codes = generate_codes(args.length, stop_if_seen=args.stop_if_seen)
+    if args.alpha_chars:
+        codes = (get_code(args.length, alphanum_chars=list(args.alpha_chars.upper()))
+                 for i in range(args.number))
+    else:
+        codes = generate_codes(args.length, stop_if_seen=args.stop_if_seen)
 
     for i in range(args.number):
         args.outfile.write(next(codes) + '\n')
